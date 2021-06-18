0b63e14399e89b5bf3416ebbf6a420d644c53751
TimberKito <554080105@qq.com>
2021-06-18 23:11:41 +0800

    releases 1.0

--- src/main/java/com/timberkito/shop/controller/CustomerController.java
+++ src/main/java/com/timberkito/shop/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.timberkito.shop.controller;
 import com.timberkito.shop.model.Customer;
 import com.timberkito.shop.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,46 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 //@Controller
+@CrossOrigin
 @RestController // 此类所有方法返回JSON
 public class CustomerController{
 
     @Autowired
     private CustomerService customerService;
 
-    /**
-     * 查询所有客户
-     *
-     * @return
-     */
     @RequestMapping(value = "/api/customer/selectall", method = RequestMethod.GET)
     public List selectAll(){
         return customerService.selectAllCustomers();
     }
 
-    /**
-     * 查询指定id客户
-     *
-     * @param id
-     * @return
-     */
     @RequestMapping(value = "/api/customer/info", method = RequestMethod.GET)
     public Object customerInfo(Integer id){
         Customer customer = customerService.queryCustomerById(id);
         return customer;
     }
 
-    /**
-     * 添加客户
-     *
-     * @param id
-     * @param username
-     * @param jobs
-     * @param phone
-     * @return
-     */
     @RequestMapping(value = "/api/customer/add", method = RequestMethod.POST)
-    public int add(Integer id, String username, String jobs, String phone){
-        Customer customer = new Customer(id, username, jobs, phone);
+    public int add(Customer customer){
         int ret = customerService.insertSelective(customer);
         if (ret == 1) {
             return 200;
@@ -58,13 +39,6 @@ public class CustomerController{
         }
     }
 
-
-    /**
-     * 删除指定id客户
-     *
-     * @param id
-     * @return
-     */
     @RequestMapping(value = "/api/customer/delete", method = RequestMethod.POST)
     public int deleteByPrimaryKey(Integer id){
         int ret = customerService.deleteByPrimaryKey(id);
@@ -75,19 +49,8 @@ public class CustomerController{
         }
     }
 
-
-    /**
-     * 修改用户
-     *
-     * @param id
-     * @param username
-     * @param jobs
-     * @param phone
-     * @return
-     */
     @RequestMapping(value = "/api/customer/update", method = RequestMethod.POST)
-    public int update(Integer id, String username, String jobs, String phone){
-        Customer customer = new Customer(id, username, jobs, phone);
+    public int update(Customer customer){
         int ret = customerService.updateByPrimaryKeySelective(customer);
         if (ret == 1) {
             return 200;
